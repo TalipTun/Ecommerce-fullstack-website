ae384c41f6113271b23a8cbf5cb9c162bc5c12b1
TalipTun <taliptun62@gmail.com>
2025-01-30 20:29:50 +0000

    added search bar

--- Ecommerce-website/src/Navbar.tsx
+++ Ecommerce-website/src/Navbar.tsx
@@ -1,20 +1,18 @@
 function Navbar() {
     return (
       <>
-        <nav className="fixed top-0 left-0 right-0 w-full border-b border-green-400 h-24 flex flex-row items-center justify-between px-48">
+        <nav className="fixed top-0 left-0 right-0 w-full border-b border-green-400 h-24 flex flex-row items-center justify-between px-36 gap-16">
             <div>
                 <h1 className="flex items-center justify-center bg-gradient-to-r from-green-400 to-green-900 text-gray-150 
         text-2xl font-bold shadow-lg rounded-xl hover:scale-105 transition-transform duration-300
                 w-32 h-16 text-center align-middle">Talipify</h1>
             </div>
-            <div>
-                <ul className="flex flex-row items-center h-full">
+            <div className="w-full">
                 <input
                   type="text"
-                  placeholder="Search..."
-                  className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Search products"
+                  className="w-full p-2 border border-black-800 rounded-4xl focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
-                </ul>
             </div>
             <div>
                 <p className="text-green-800">cart</p>
