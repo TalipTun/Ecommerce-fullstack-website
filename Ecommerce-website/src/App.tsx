22021f148ba38e2c5642b53b6a5bb01f07deffc6
TalipTun <taliptun62@gmail.com>
2025-02-01 17:22:44 +0000

    added a function to fetch API and logged it into the console

--- Ecommerce-website/src/App.tsx
+++ Ecommerce-website/src/App.tsx
@@ -1,11 +1,23 @@
 import './App.css'
 import Navbar from './Navbar.tsx'
+import {getItems} from "./api.ts"
+import { useEffect } from "react";  // Import React and useEffect
+
 function App() {
+  useEffect(() => {
+    async function fetchData() {
+      const item = await getItems();  
+      console.log(item);  
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
-      <Navbar/>
+      <Navbar />
     </>
-  )
+  );
 }
+
 
 export default App
