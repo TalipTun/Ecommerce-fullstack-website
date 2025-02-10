829fbcfd9798829fba684e9d60bbeb22a412d74c
TalipTun <taliptun62@gmail.com>
2025-02-10 14:23:53 +0000

    created the auth.js file and coded it

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -4,3 +4,25 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const router = express.Router();
+
+router.post("/register", async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        const user = new User({ name, email, password});
+        await user.save();
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(500).json({ message: "Error registering user"})
+    }
+});
+
+router.get("/users", async(req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: "Error fetching users"});
+    }
+});
+
+export default router;
